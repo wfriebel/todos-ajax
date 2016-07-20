00c96176cd5d524ab5783c1c34c5cc3b340f9a97
[01;31m[K"name"[m[K: "Jaclyn", &   [01;31m[K"name"[m[K: null <pair+jkarnowski.yourgithubusername@devbootcamp.com>
2016-07-20 08:57:50 -0700

    removes functions for breakout;

--- public/js/application.js
+++ public/js/application.js
@@ -1,28 +1,5 @@
 $(document).ready(function() {
   console.log('READY');
 
-  getNewTodoFormListener();
 });
 
-function getNewTodoFormListener(){
-  $('#new-todo-form').on('click', function(event){
-    event.preventDefault();
-    console.log('clicked it');
-    displayForm();
-  })
-}
-
-function displayForm(){
-  $.ajax({
-    url: '/todos/new',
-    method: 'GET'
-  })
-  .done(function(response){
-    console.log('respon', response)
-    $('.todos').hide();
-    $('#new-todo').append(response)
-  })
-  .fail(function(response){
-    console.log('failling')
-  })
-}
